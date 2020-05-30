c288b1a454c4c37a8fe40f9c8d5c12e52892cff2
Adrian Diaz <web.art@adriandiaz.ca>
2020-05-30 09:44:43 -0400

    add basic gql schema and resolver

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 const port = process.env.PORT || 8080
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    return res.status(200).send({"status": "OK"})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Cycling','Coding', 'Gardening']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}), (req, res, next) => {
+
 });
 
 app.listen(port, () =>{
