275f305dbc0ac94f2e9b519100e7f35a130f706e
Adrian Diaz <web.art@adriandiaz.ca>
2020-05-30 22:22:53 -0400

    add graphql mutation

--- app.js
+++ app.js
@@ -6,16 +6,34 @@ const { buildSchema } = require('graphql');
 const port = process.env.PORT || 8080
 
 const app = express();
+
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -25,17 +43,23 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return ['Cycling','Coding', 'Gardening']
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                desciption: args.eventInput.description,
+                price: +args.eventInput.price, 
+                date: args.eventInput.eventInputdate
+            }
+            events.push(event)
+            return event;
         }
     },
     graphiql: true
-}), (req, res, next) => {
-
-});
+})
+);
 
 app.listen(port, () =>{
     console.log('Listening on http://localhost:' + port)
