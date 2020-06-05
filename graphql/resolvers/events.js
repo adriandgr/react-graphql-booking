af1905fc0d396c2e1f362c935b70d1630f45bee9
Adrian Diaz <web.art@adriandiaz.ca>
2020-06-04 23:19:27 -0400

    split resolvers

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,38 @@
+const Event = require('../../models/event');
+const { transformEvent } = require('./merge');
+
+
+module.exports = {
+    events: async () => {
+        try {
+            const events = await Event.find()
+            return events.map(transformEvent);
+        } catch (err) {
+            throw err;
+        }
+    },
+    createEvent: async args => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price, 
+            date: new Date(args.eventInput.date),
+            creator: '5ed32ddb0e563f8540856dc8'
+        });
+        let createdEvent;
+        try {
+            const result = await event.save()
+            createdEvent = transformEvent(result)
+            const creator = await User.findById('5ed32ddb0e563f8540856dc8')
+            if (!creator) {
+                throw new Error('User not found.')
+            }
+            creator.createdEvents.push(event);
+            await creator.save();
+            return createdEvent;
+        } catch (err)  {
+            console.log(err);
+            throw err;
+        }
+    },
+}
